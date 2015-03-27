95961cb2954e22f1a67ca6789af21e5aeb22de57
Shawn Mclean <shawnrmclean@gmail.com>
2015-03-26 22:46:30 -0500

    update description

--- src/Eventbrite/Properties/AssemblyInfo.cs
+++ src/Eventbrite/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Eventbrite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple API wrapper for Evenbrite")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xormis")]
 [assembly: AssemblyProduct("Eventbrite")]
