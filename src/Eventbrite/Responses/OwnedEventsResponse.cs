d2f7a0f1fa87bffc277ef2f448e44fcccf6c9248
Shawn Mclean <shawnrmclean@gmail.com>
2015-03-31 00:16:03 -0500

    added missing files

---
+++ src/Eventbrite/Responses/OwnedEventsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eventbrite.Entities;
+using Newtonsoft.Json;
+
+namespace Eventbrite.Responses
+{
+  public class OwnedEventsResponse
+  {
+    [JsonProperty(PropertyName = "pagination")]
+    public Pagination Pagination { get; set; }
+
+    [JsonProperty(PropertyName = "events")]
+    public IList<Event> Events { get; set; }
+  }
+}
