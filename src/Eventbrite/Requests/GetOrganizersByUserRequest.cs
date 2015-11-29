6eb52810a3d165722efadd3098ddbcb48d05450c
Matt Chatterley <matt@code.je>
2015-11-29 19:47:37 +0000

    Add support for retrieving full list of organisers by user

---
+++ src/Eventbrite/Requests/GetOrganizersByUserRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Eventbrite.Entities;
+using Eventbrite.Responses;
+
+namespace Eventbrite.Requests
+{
+    public class GetOrganizersByUserRequest : IRequest<OrganizersResponse>
+    {
+        public GetOrganizersByUserRequest(long userId)
+        {
+            this.UserId = userId;
+        }
+
+        public long UserId { get; private set; }
+        public HttpMethod HttpMethod { get { return HttpMethod.Get; } }
+        public string RequestUri { get { return string.Format("/v3/users/{0}/organizers", this.UserId); } }
+    }
+}
---
+++ src/Eventbrite/Responses/OrganizersResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eventbrite.Entities;
+using Newtonsoft.Json;
+
+namespace Eventbrite.Responses
+{
+    public class OrganizersResponse
+    {
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; set; }
+
+        [JsonProperty(PropertyName = "organizers")]
+        public IList<Organizer> Organizers { get; set; }
+    }
+}
